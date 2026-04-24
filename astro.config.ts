8933878dbdd5367b250e583215d00f77617d4c56
Madhu Pandey <madhupandey29052004@gmail.com>
2026-04-24 12:26:23 +0530

    remove unused packages , audit fix , remove hardcoded company name in pages title

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import tailwindcss from '@tailwindcss/vite';
 import { loadEnv } from 'vite';
@@ -27,7 +26,6 @@ export default defineConfig({
   redirects,
   site: process.env.PUBLIC_SITE_URL ?? 'http://localhost:4321',
   integrations: [
-    mdx(),
     cloudinaryPicture(),
     partytown({ config: { forward: ['dataLayer.push', 'gtag', 'clarity'] } }),
   ],
